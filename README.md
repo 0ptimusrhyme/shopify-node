2751009059eb80ba63517b96fb2f3c522d3b0e19
0ptimusrhyme <alex.richter@shopify.com>
2017-09-20 16:41:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Example Shopify Node App
+A simple app built to gain a base understanding of interfacing with Shopify's API in a NodeJS context.
+
+## Install
+1. Create a [Shopify Partner Account](https://partners.shopify.com)
+2. Create a partner app with generated API credentials
+3. From the root directory: `npm init`
+4. `npm install express dotenv request request-promise --save`
+5. `touch .env`
+6. Add your credentials and app url to the `.env` file
+7. Visit your app url with a `shop` param.
+
+For greater base familiarity with Shopify's API, I recommend having the [documentation](https://developers.shopify.com/) available for reference if needed.
---
+++ index.js
@@ -0,0 +1,78 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+const crypto = require('crypto');
+const querystring = require('querystring');
+const rp = require('request-promise');
+const pry = require('pryjs')
+
+// Get data from .env
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.API_SECRET;
+const webAddress = process.env.WEB_ADDRESS;
+
+// GET root route
+app.get('/', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const redirectUrl = webAddress + "auth";
+    const scope = 'write_products';
+    const oauthUrl = "https://" + shop +
+    "/admin/oauth/authorize?client_id=" + apiKey +
+    "&redirect_uri=" + redirectUrl +
+    "&scope=" + scope;
+
+    return res.redirect(oauthUrl);
+  } else {
+    return res.status(400).send('Missing shop param.')
+  }
+});
+
+// GET auth route
+app.get('/auth', (req, res) => {
+  // Validate hmac
+  const hmac = req.query.hmac;
+  const code = req.query.code;
+  const shop = req.query.shop
+
+  if (shop && hmac && code) {
+    const queryParams = req.query;
+    delete queryParams['signature'];
+    delete queryParams['hmac'];
+    const message = querystring.stringify(queryParams);
+    const generatedHash = crypto.createHmac('sha256', apiSecret).update(message).digest('hex');
+
+    if (generatedHash !== hmac) {
+      res.status(400).send('HMAC validation failed');
+    }
+
+    const accessTokenUrl = "https://" + shop + "/admin/oauth/access_token";
+    const tokenPayload = {
+      client_id: apiKey,
+      client_secret: apiSecret,
+      code
+    };
+
+    rp.post(accessTokenUrl, { json: tokenPayload })
+    .then((accessTokenResponse) => {
+      const accessToken = accessTokenResponse.access_token;
+      const shopRequestURL = "https://" + shop + "/admin/shop.json";
+      const shopRequestHeaders = { 'x-shopify-access-token': accessToken };
+      rp.get(shopRequestURL, { headers: shopRequestHeaders })
+      .then((shopResponse) => {
+        res.send(shopResponse)
+        .catch((error) => {
+          res.status(error.statusCode).send(error.error.error_description);
+        });
+      });
+    }).catch((error) => {
+      res.status(error.statusCode).send(error.error.error_description);
+    });
+  } else {
+    res.status(400).send('Required parameters missing')
+  }
+});
+
+app.listen(4567, () => {
+  console.log('Example app listening on port 4567!');
+});
